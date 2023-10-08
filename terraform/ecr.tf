464d2e2658be1bb8e77cdc466bc81e416f093c51
Anthony Viera <anthonyviera4@gmail.com>
2023-10-07 21:59:25 -0500

    Tf surgery

---
+++ terraform/ecr.tf
@@ -0,0 +1,11 @@
+resource "aws_ecr_repository" "paradise_cakes_api_lambda" {
+  name                 = "paradise-cakes-api-us-east-1"
+  image_tag_mutability = "MUTABLE"
+  encryption_configuration {
+    encryption_type = "AES256"
+  }
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
