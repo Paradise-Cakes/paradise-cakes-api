4eb93c49225a0bdfd836f1763b3b59bc1e9be2ed
Anthony Viera <anthonyviera4@gmail.com>
2023-10-08 20:30:53 -0500

    Wat about now

--- terraform/acm.tf
+++ terraform/acm.tf
@@ -1,15 +1,28 @@
 resource "aws_acm_certificate" "paradise_cakes_cloud" {
   domain_name       = "api.paradisecakes.cloud"
   validation_method = "DNS"
-
-  lifecycle {
-    create_before_destroy = true
-  }
 }
 
-data "aws_route53_zone" "public" {
+data "aws_route53_zone" "paradise_cakes_cloud" {
   name         = "paradisecakes.cloud"
   private_zone = false
+}
+
+resource "aws_route53_record" "paradise_cakes_cloud" {
+  for_each = {
+    for dvo in aws_acm_certificate.paradise_cakes_cloud.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = data.aws_route53_zone.paradise_cakes_cloud.zone_id
 }
 
 resource "aws_acm_certificate_validation" "paradise_cakes_cloud" {
