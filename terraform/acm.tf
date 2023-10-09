d8149c93eadac7e2ad5967b65f41eb107ad12f8b
Anthony Viera <anthonyviera4@gmail.com>
2023-10-08 19:03:38 -0500

    Fix i think

--- terraform/acm.tf
+++ terraform/acm.tf
@@ -1,5 +1,5 @@
-resource "aws_acm_certificate" "cakes_api_tech" {
-  domain_name       = "cakes.api.tech"
+resource "aws_acm_certificate" "paradise_cakes_cloud" {
+  domain_name       = "paradisecakes.cloud"
   validation_method = "DNS"
 
   lifecycle {
@@ -7,19 +7,6 @@ resource "aws_acm_certificate" "cakes_api_tech" {
   }
 }
 
-resource "aws_acm_certificate_validation" "cakes_api_tech" {
-  certificate_arn = aws_acm_certificate.cakes_api_tech.arn
-}
-
-resource "aws_acm_certificate" "api" {
-  domain_name       = "cakes-us-east-1.api.tech"
-  validation_method = "DNS"
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
-resource "aws_acm_certificate_validation" "api" {
-  certificate_arn = aws_acm_certificate.api.arn
+resource "aws_acm_certificate_validation" "paradise_cakes_cloud" {
+  certificate_arn = aws_acm_certificate.paradise_cakes_cloud.arn
 }
