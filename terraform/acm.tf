9fc83c729db0a6a1db9322265b802d232159d0c9
Anthony Viera <anthonyviera4@gmail.com>
2023-10-08 19:50:27 -0500

    I have no idea what im doing

--- terraform/acm.tf
+++ terraform/acm.tf
@@ -1,10 +1,15 @@
 resource "aws_acm_certificate" "paradise_cakes_cloud" {
-  domain_name       = "paradisecakes.cloud"
+  domain_name       = "api.paradisecakes.cloud"
   validation_method = "DNS"
 
   lifecycle {
     create_before_destroy = true
   }
+}
+
+data "aws_route53_zone" "public" {
+  name         = "paradisecakes.cloud"
+  private_zone = false
 }
 
 resource "aws_acm_certificate_validation" "paradise_cakes_cloud" {
