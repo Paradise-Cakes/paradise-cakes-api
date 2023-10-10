457c39c6c34cd9ce0e2f59e060fdbd45b9ca501a
Anthony Viera <anthonyviera4@gmail.com>
2023-10-09 23:18:01 -0500

    maybe

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -15,3 +15,23 @@ resource "aws_iam_role" "paradise_cakes_api_role" {
   name               = "paradise-cakes-api-role"
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
+
+resource "aws_iam_role_policy" "paradise_cakes_api_role_policy" {
+  name = "paradise-cakes-api-role_policy"
+  role = aws_iam_role.paradise_cakes_api_role.id
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "lambda:InvokeFunction",
+      ],
+      "Resource": ["*"]
+    }
+  ]
+}
+EOF
+}
