80dccb7cf7ec779c589c025578899f57e7fde094
Anthony Viera <anthonyviera4@gmail.com>
2023-10-10 22:45:57 -0500

    That should do it

--- terraform/elb.tf
+++ terraform/elb.tf
@@ -1,6 +1,6 @@
 resource "aws_elb" "main" {
   name               = "paradise-cakes-elb"
-  availability_zones = ["us-east-1a, us-east-1b, us-east-1c"]
+  availability_zones = ["us-east-1a", "us-east-1b", "us-east-1c"]
 
   listener {
     instance_port     = 80
