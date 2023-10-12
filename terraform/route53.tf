eaddf3470491f6e5cf0823090528efe31fde764c
Anthony Viera <anthonyviera4@gmail.com>
2023-10-11 22:04:58 -0500

    im dum

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -8,8 +8,8 @@ resource "aws_route53_record" "main" {
   type    = "A"
 
   alias {
-    name                   = aws_api_gateway_domain_name.domain_name.cloudfront_domain_name
-    zone_id                = aws_api_gateway_domain_name.domain_name.cloudfront_zone_id
+    name                   = aws_api_gateway_domain_name.paradise_cakes_cloud.cloudfront_domain_name
+    zone_id                = aws_api_gateway_domain_name.paradise_cakes_cloud.cloudfront_zone_id
     evaluate_target_health = true
   }
 }
