801e4cf17114ae0563bac2480c10257ae5a5c3c2
Anthony Viera <anthonyviera4@gmail.com>
2023-10-11 20:32:34 -0500

    fail again i dare you

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -2,7 +2,7 @@ resource "aws_lb" "paradise_cakes_lb" {
   name               = "paradise-cakes-lb"
   internal           = false
   load_balancer_type = "application"
-  subnets            = [aws_subnet.public_subnet_az1.vpc_id]
+  subnets            = [aws_subnet.public_subnet_az1.id]
 }
 
 resource "aws_lb_target_group" "paradise_cakes_target_group" {
