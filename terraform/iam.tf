0b8caf9aad15643887f3a32442cc307d1b076268
Anthony Viera <anthonyviera4@gmail.com>
2023-10-11 19:49:17 -0500

    Will this pass

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,37 +1,41 @@
-data "aws_iam_policy_document" "assume_role" {
-  statement {
-    effect = "Allow"
+resource "aws_iam_role" "lambda_execution_role" {
+  name = "lambda-execution-role"
 
-    principals {
-      type        = "Service"
-      identifiers = ["lambda.amazonaws.com"]
-    }
-
-    actions = ["sts:AssumeRole"]
-  }
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action = "sts:AssumeRole",
+      Effect = "Allow",
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+    }]
+  })
 }
 
-resource "aws_iam_role" "paradise_cakes_api_role" {
-  name               = "paradise-cakes-api-role"
-  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+resource "aws_iam_policy" "paradise_cakes_api_policy" {
+  name        = "paradise-cakes-api-policy"
+  description = "paradise cakes api policy"
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = [
+          "execute-api:Invoke"
+        ],
+        Effect   = "Allow",
+        Resource = "arn:aws:execute-api:us-east-1:${data.aws_caller_identity.current.account_id}:*/*/*/*"
+      },
+      {
+        Action   = "lambda:InvokeFunction",
+        Effect   = "Allow",
+        Resource = aws_lambda_function.app.arn
+      }
+    ]
+  })
 }
 
-resource "aws_iam_role_policy" "paradise_cakes_api_role_policy" {
-  name = "paradise-cakes-api-role_policy"
-  role = aws_iam_role.paradise_cakes_api_role.id
-
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Action": [
-        "lambda:*"
-      ],
-      "Resource": "*"
-    }
-  ]
-}
-EOF
+resource "aws_iam_role_policy_attachment" "api_gateway_attachment" {
+  policy_arn = aws_iam_policy.paradise_cakes_api_policy.arn
+  role       = aws_iam_role.lambda_execution_role.name
 }
--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -2,7 +2,7 @@ resource "aws_lambda_function" "app" {
   image_uri     = "${aws_ecr_repository.paradise_cakes_api_lambda.repository_url}:${var.docker_image_tag}"
   package_type  = "Image"
   function_name = "paradise-cakes-api-us-east-1"
-  role          = aws_iam_role.paradise_cakes_api_role.arn
+  role          = aws_iam_role.lambda_execution_role.arn
 
   timeout     = 30
   memory_size = 1024
