319643839bd9c8faf1d8b875a2907d7a053486f4
Anthony Viera <anthonyviera4@gmail.com>
2023-10-11 21:48:06 -0500

    What about this

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -2,14 +2,14 @@ data "aws_route53_zone" "hosted_zone" {
   name = "paradisecakes.cloud"
 }
 
-resource "aws_route53_record" "www" {
+resource "aws_route53_record" "main" {
   zone_id = data.aws_route53_zone.hosted_zone.zone_id
-  name    = "www.paradisecakes.cloud"
+  name    = "paradisecakes.cloud"
   type    = "A"
 
   alias {
-    name                   = aws_lb.paradise_cakes_lb.dns_name
-    zone_id                = aws_lb.paradise_cakes_lb.zone_id
+    name                   = aws_api_gateway_domain_name.domain_name.cloudfront_domain_name
+    zone_id                = aws_api_gateway_domain_name.domain_name.cloudfront_zone_id
     evaluate_target_health = true
   }
 }
