ab31b352deab2f86f6972de1b734793fa9c235ff
Anthony Viera <anthonyviera4@gmail.com>
2024-03-05 00:57:27 -0600

    there

--- lambdas/src/api.py
+++ lambdas/src/api.py
@@ -1,12 +1,12 @@
 from mangum import Mangum
 from fastapi import FastAPI
-from src.routes import hello
+from src.routes import hello, get_desserts
 
 app = FastAPI(title="Paradise Cakes API", version="1.0.0", root_path="/v1")
 
 
 app.include_router(hello.router)
-
+app.include_router(get_desserts.router)
 
 def lambda_handler(event, context):
     handler = Mangum(app, lifespan="on", api_gateway_base_path="/v1")
