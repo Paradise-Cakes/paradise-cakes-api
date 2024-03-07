858819fcc38e3e607dd78423e2f5202d5b178cde
Anthony Viera <anthonyviera4@gmail.com>
2024-03-06 19:03:18 -0600

    add origins

--- lambdas/src/api.py
+++ lambdas/src/api.py
@@ -4,6 +4,19 @@ from src.routes import hello, get_desserts
 
 app = FastAPI(title="Paradise Cakes API", version="1.0.0", root_path="/v1")
 
+origins = [
+    "http://localhost*
+    "https://paradisecakesbymegan.com/"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(hello.router)
 app.include_router(get_desserts.router)
