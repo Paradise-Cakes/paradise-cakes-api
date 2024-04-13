4e006140cc07d8634c808bebf3ace3416c90eeb0
Anthony Viera <anthonyviera96@gmail.com>
2024-04-12 19:08:13 -0500

    NO

--- terraform/apigateway.tf
+++ terraform/apigateway.tf
@@ -42,15 +42,6 @@ resource "aws_api_gateway_base_path_mapping" "path_mapping_internal" {
   base_path   = aws_api_gateway_stage.paradise_cakes.stage_name
 }
 
-resource "aws_api_gateway_integration" "paradise_cakes_integration" {
-  rest_api_id             = aws_api_gateway_rest_api.paradise_cakes_api.id
-  resource_id             = aws_api_gateway_resource.proxy.id
-  http_method             = aws_api_gateway_method.paradise_cakes_proxy.http_method
-  integration_http_method = "POST"
-  type                    = "AWS_PROXY"
-  uri                     = aws_lambda_function.app.invoke_arn
-}
-
 resource "aws_api_gateway_rest_api_policy" "paradise_cakes_api" {
   rest_api_id = aws_api_gateway_rest_api.paradise_cakes_api.id
 
