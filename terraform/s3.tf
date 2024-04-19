f8bba3e320c800f6d56d8eae3a71bf4865b8b125
Anthony Viera <anthonyviera96@gmail.com>
2024-04-18 23:26:28 -0500

    Update cors for bukkit

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,3 +1,20 @@
 resource "aws_s3_bucket" "pc_dessert_images_bucket" {
   bucket = "pc-dessert-images-bucket"
 }
+
+resource "aws_s3_bucket_cors_configuration" "pc_dessert_images_bucket_cors" {
+  bucket = aws_s3_bucket.pc_dessert_images_bucket.id
+
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["PUT", "POST"]
+    allowed_origins = ["*"]
+    expose_headers  = ["ETag"]
+    max_age_seconds = 3000
+  }
+
+  cors_rule {
+    allowed_methods = ["GET"]
+    allowed_origins = ["*"]
+  }
+}
