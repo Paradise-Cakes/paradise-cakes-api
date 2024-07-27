9ae43d5d844d9fc66d29f776b665e5329862de07
Anthony Viera <anthonyviera4@gmail.com>
2024-07-27 18:20:45 -0500

    ok you got me

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -5,24 +5,12 @@ data "aws_route53_zone" "hosted_zone" {
 
 resource "aws_route53_record" "main" {
   zone_id = data.aws_route53_zone.hosted_zone.zone_id
-  name    = "paradisecakes.cloud"
+  name    = var.environment == "prod" ? "paradisecakes.cloud" : "paradisecakes-dev.cloud"
   type    = "A"
 
   alias {
     name                   = aws_api_gateway_domain_name.paradise_cakes_cloud.cloudfront_domain_name
     zone_id                = aws_api_gateway_domain_name.paradise_cakes_cloud.cloudfront_zone_id
-    evaluate_target_health = true
-  }
-}
-
-resource "aws_route53_record" "dev" {
-  zone_id = data.aws_route53_zone.hosted_zone.zone_id
-  name    = "paradisecakes-dev.cloud"
-  type    = "A"
-
-  alias {
-    name                   = aws_api_gateway_domain_name.paradise_cakes_cloud_dev.cloudfront_domain_name
-    zone_id                = aws_api_gateway_domain_name.paradise_cakes_cloud_dev.cloudfront_zone_id
     evaluate_target_health = true
   }
 }
