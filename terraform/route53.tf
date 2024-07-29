09c7807e8d18ac2df98fc2c2a1ce19afac6b4cfc
Anthony Viera <anthonyviera4@gmail.com>
2024-07-28 23:25:17 -0500

    Welp

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -8,24 +8,12 @@ data "aws_route53_zone" "paradise_cakes" {
 
 resource "aws_route53_record" "api_record" {
   zone_id = data.aws_route53_zone.paradise_cakes.zone_id
-  name    = "api.paradisecakesbymegan.com"
+  name    = var.environment == "prod" ? "api.paradisecakesbymegan.com" : "dev-api.paradisecakesbymegan.com"
   type    = "A"
 
   alias {
     name                   = aws_api_gateway_domain_name.api.cloudfront_domain_name
     zone_id                = aws_api_gateway_domain_name.api.cloudfront_zone_id
-    evaluate_target_health = true
-  }
-}
-
-resource "aws_route53_record" "dev_api_record" {
-  zone_id = data.aws_route53_zone.paradise_cakes.zone_id
-  name    = "dev-api.paradisecakesbymegan.com"
-  type    = "A"
-
-  alias {
-    name                   = aws_api_gateway_domain_name.dev_api.cloudfront_domain_name
-    zone_id                = aws_api_gateway_domain_name.dev_api.cloudfront_zone_id
     evaluate_target_health = true
   }
 }
