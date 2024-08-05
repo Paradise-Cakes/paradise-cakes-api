90d9bcc94e0200c76f84bf9c7af6185186c7bcc5
Anthony Viera <anthonyviera4@gmail.com>
2024-08-04 22:46:39 -0500

    ns records

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -1,9 +1,9 @@
-resource "aws_route53_zone" "paradise_cakes" {
+resource "aws_route53_zone" "paradise_cakes_api" {
   name = var.environment == "prod" ? "api.paradisecakesbymegan.com" : "dev-api.paradisecakesbymegan.com"
 }
 
 resource "aws_route53_record" "api_record" {
-  zone_id = aws_route53_zone.paradise_cakes.zone_id
+  zone_id = aws_route53_zone.paradise_cakes_api.zone_id
   name    = var.environment == "prod" ? "api.paradisecakesbymegan.com" : "dev-api.paradisecakesbymegan.com"
   type    = "A"
 
@@ -20,7 +20,7 @@ resource "aws_route53_record" "paradise_cakes_validation_record" {
       name    = dvo.resource_record_name
       record  = dvo.resource_record_value
       type    = dvo.resource_record_type
-      zone_id = aws_route53_zone.paradise_cakes.zone_id
+      zone_id = aws_route53_zone.paradise_cakes_api.zone_id
     }
   }
 
@@ -30,4 +30,23 @@ resource "aws_route53_record" "paradise_cakes_validation_record" {
   ttl             = 60
   type            = each.value.type
   zone_id         = each.value.zone_id
+}
+
+data "aws_route53_zone" "paradise_cakes" {
+  name         = var.environment == "prod" ? "paradisecakesbymegan.com" : "dev.paradisecakesbymegan.com"
+  private_zone = false
+}
+
+resource "aws_route53_record" "paradise_cakes_api_ns" {
+  zone_id = data.aws_route53_zone.paradise_cakes.zone_id
+  name    = var.environment == "prod" ? "api.paradisecakesbymegan.com" : "dev-api.paradisecakesbymegan.com"
+  type    = "NS"
+  ttl     = 300
+
+  records = [
+    data.aws_route53_zone.paradise_cakes.name_servers[0],
+    data.aws_route53_zone.paradise_cakes.name_servers[1],
+    data.aws_route53_zone.paradise_cakes.name_servers[2],
+    data.aws_route53_zone.paradise_cakes.name_servers[3],
+  ]
 }
