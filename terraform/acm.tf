05f59b6213a2c67601d3dd47bb3433ff20d03eba
Anthony Viera <anthonyviera4@gmail.com>
2024-08-04 22:25:01 -0500

    hope this works

--- terraform/acm.tf
+++ terraform/acm.tf
@@ -1,15 +1,9 @@
 resource "aws_acm_certificate" "paradise_cakes" {
-  count             = var.environment == "prod" ? 1 : 0
-  domain_name       = "api.paradisecakesbymegan.com"
+  domain_name       = var.environment == "prod" ? "api.paradisecakesbymegan.com" : "dev-api.paradisecakesbymegan.com"
   validation_method = "DNS"
 }
 
-data "aws_acm_certificate" "paradise_cakes_dev" {
-  count  = var.environment == "prod" ? 0 : 1
-  domain = "dev-api.paradisecakesbymegan.com"
-}
-
 resource "aws_acm_certificate_validation" "paradise_cakes" {
-  count           = var.environment == "prod" ? 1 : 0
-  certificate_arn = aws_acm_certificate.paradise_cakes[0].arn
+  certificate_arn         = aws_acm_certificate.paradise_cakes.arn
+  validation_record_fqdns = [for record in aws_route53_record.paradise_cakes_validation_record : record.fqdn]
 }
--- terraform/route53.tf
+++ terraform/route53.tf
@@ -1,39 +1,33 @@
-provider "aws" {
-  region = "us-east-1"
-}
-
-data "aws_route53_zone" "paradise_cakes" {
-  count = var.environment == "prod" ? 1 : 0
-  name  = "paradisecakesbymegan.com"
+resource "aws_route53_zone" "paradise_cakes" {
+  name = var.environment == "prod" ? "api.paradisecakesbymegan.com" : "dev-api.paradisecakesbymegan.com"
 }
 
 resource "aws_route53_record" "api_record" {
-  count   = var.environment == "prod" ? 1 : 0
-  zone_id = data.aws_route53_zone.paradise_cakes[0].zone_id
-  name    = "api.paradisecakesbymegan.com"
+  zone_id = aws_route53_zone.paradise_cakes.zone_id
+  name    = var.environment == "prod" ? "api.paradisecakesbymegan.com" : "dev-api.paradisecakesbymegan.com"
   type    = "A"
 
   alias {
-    name                   = aws_api_gateway_domain_name.api[0].cloudfront_domain_name
-    zone_id                = aws_api_gateway_domain_name.api[0].cloudfront_zone_id
+    name                   = aws_api_gateway_domain_name.api.cloudfront_domain_name
+    zone_id                = aws_api_gateway_domain_name.api.cloudfront_zone_id
     evaluate_target_health = true
   }
 }
 
-data "aws_api_gateway_domain_name" "api_dev" {
-  count       = var.environment == "prod" ? 1 : 0
-  domain_name = "dev-api.paradisecakesbymegan.com"
-}
-
-resource "aws_route53_record" "api_record_dev" {
-  count   = var.environment == "prod" ? 1 : 0
-  zone_id = data.aws_route53_zone.paradise_cakes[0].zone_id
-  name    = "dev-api.paradisecakesbymegan.com"
-  type    = "A"
-
-  alias {
-    name                   = data.aws_api_gateway_domain_name.api_dev[0].cloudfront_domain_name
-    zone_id                = data.aws_api_gateway_domain_name.api_dev[0].cloudfront_zone_id
-    evaluate_target_health = true
+resource "aws_route53_record" "paradise_cakes_validation_record" {
+  for_each = {
+    for dvo in aws_acm_certificate.paradise_cakes.domain_validation_options : dvo.domain_name => {
+      name    = dvo.resource_record_name
+      record  = dvo.resource_record_value
+      type    = dvo.resource_record_type
+      zone_id = data.aws_route53_zone.paradise_cakes.zone_id
+    }
   }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = each.value.zone_id
 }
