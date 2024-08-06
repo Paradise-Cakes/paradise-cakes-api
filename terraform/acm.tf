30c6fd2fc5aa305130b0450d22c4160e83dca97d
Anthony Viera <anthonyviera4@gmail.com>
2024-08-05 21:19:01 -0500

    dont use the acm cert until its verified

--- terraform/acm.tf
+++ terraform/acm.tf
@@ -1,11 +1,10 @@
 resource "aws_acm_certificate" "paradise_cakes" {
   domain_name       = var.environment == "prod" ? "api.paradisecakesbymegan.com" : "dev-api.paradisecakesbymegan.com"
   validation_method = "DNS"
+  depends_on        = [aws_route53_record.paradise_cakes_api_ns]
 }
 
 resource "aws_acm_certificate_validation" "paradise_cakes" {
   certificate_arn         = aws_acm_certificate.paradise_cakes.arn
   validation_record_fqdns = [for record in aws_route53_record.paradise_cakes_validation_record : record.fqdn]
-
-  depends_on = [aws_route53_record.paradise_cakes_api_ns]
 }
