9ef595122e585b03ed99086ff07c06f37fe8f23b
Anthony Viera <anthonyviera4@gmail.com>
2024-08-10 21:13:22 -0500

    Lets see what this does

--- terraform/cognito.tf
+++ terraform/cognito.tf
@@ -76,3 +76,74 @@ resource "aws_cognito_user_group" "paradise_cakes_admin_group" {
   description  = "paradise cakes admin group"
   user_pool_id = aws_cognito_user_pool.paradise_cakes_user_pool.id
 }
+
+resource "aws_cognito_user_pool" "dev_paradise_cakes_user_pool" {
+  name = "dev-paradise-cakes-user-pool"
+
+  admin_create_user_config {
+    allow_admin_create_user_only = true
+  }
+
+  password_policy {
+    minimum_length    = 8
+    require_lowercase = true
+    require_numbers   = true
+    require_symbols   = true
+    require_uppercase = true
+  }
+
+  mfa_configuration = "OFF" # or "ON" if you want MFA
+
+  schema {
+    name                = "email"
+    attribute_data_type = "String"
+    mutable             = false
+    required            = true
+  }
+
+  username_attributes      = ["email"]
+  auto_verified_attributes = ["email"]
+}
+
+resource "aws_cognito_user_pool_client" "dev_paradise_cakes_client" {
+  name         = "dev-paradise-cakes-client"
+  user_pool_id = aws_cognito_user_pool.dev_paradise_cakes_user_pool.id
+
+  generate_secret = false
+
+  explicit_auth_flows = [
+    "ALLOW_USER_SRP_AUTH",
+    "ALLOW_REFRESH_TOKEN_AUTH",
+    "ALLOW_USER_PASSWORD_AUTH",
+  ]
+
+  allowed_oauth_flows_user_pool_client = true
+
+  allowed_oauth_flows = [
+    "code",
+    "implicit",
+  ]
+
+  allowed_oauth_scopes = [
+    "openid",
+    "profile",
+    "email",
+  ]
+
+  callback_urls = [
+    "https://dev.paradisecakesbymegan.com/callback",
+  ]
+
+  logout_urls = [
+    "https://dev.paradisecakesbymegan.com/logout",
+  ]
+
+  supported_identity_providers = [
+    "COGNITO",
+  ]
+}
+
+resource "aws_cognito_user_pool_domain" "dev_user_pool_domain" {
+  domain       = "dev-paradisecakesbymegan"
+  user_pool_id = aws_cognito_user_pool.dev_paradise_cakes_user_pool.id
+}
