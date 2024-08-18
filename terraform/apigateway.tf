8bc5208ba78919066715db43a5c87a090e9634cb
Anthony Viera <anthonyviera4@gmail.com>
2024-08-17 20:08:50 -0500

    YES

--- terraform/apigateway.tf
+++ terraform/apigateway.tf
@@ -1,14 +1,14 @@
 module "api_gateway" {
   source = "git@github.com:Paradise-Cakes/pc-terraform-modules.git//apiGateway"
 
-  api_name           = "paradise-cakes-api-gateway"
-  api_description    = "Proxy to handle requests to paradise cakes API"
-  binary_media_types = ["multipart/form-data"]
-  stage_name         = "v1"
-  certificate_arn    = aws_acm_certificate.paradise_cakes.arn
-  domain_name        = aws_acm_certificate.paradise_cakes.domain_name
-  lambda_function    = aws_lambda_function.app.invoke_arn
-  policy             = <<EOF
+  api_name            = "paradise-cakes-api-gateway"
+  api_description     = "Proxy to handle requests to paradise cakes API"
+  binary_media_types  = ["multipart/form-data"]
+  stage_name          = "v1"
+  certificate_arn     = aws_acm_certificate.paradise_cakes.arn
+  domain_name         = aws_acm_certificate.paradise_cakes.domain_name
+  lambda_function_arn = aws_lambda_function.app.invoke_arn
+  policy              = <<EOF
 {
   "Version": "2012-10-17",
   "Statement": [
