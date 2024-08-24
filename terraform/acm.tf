cc91300059c7667c095b3f391b1d9559f83b2e60
Anthony Viera <36007311+Anthony9698@users.noreply.github.com>
2024-08-23 21:21:46 -0500

    Use module for api gateway (#3)

--- terraform/acm.tf
+++ terraform/acm.tf
@@ -2,8 +2,3 @@ resource "aws_acm_certificate" "paradise_cakes" {
   domain_name       = var.environment == "prod" ? "api.paradisecakesbymegan.com" : "dev-api.paradisecakesbymegan.com"
   validation_method = "DNS"
 }
-
-resource "aws_acm_certificate_validation" "paradise_cakes" {
-  certificate_arn         = aws_acm_certificate.paradise_cakes.arn
-  validation_record_fqdns = [for record in aws_route53_record.paradise_cakes_validation_record : record.fqdn]
-}
