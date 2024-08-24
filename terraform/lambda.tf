e93ade6a32dc0814d5533dcc71450aff8b816092
Anthony Viera <anthonyviera4@gmail.com>
2024-08-24 16:10:32 -0500

    fix

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -24,10 +24,3 @@ resource "aws_lambda_function" "app" {
     }
   }
 }
-
-resource "aws_lambda_permission" "allow_api_gateway_handler" {
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.app.arn
-  principal     = "apigateway.amazonaws.com"
-  source_arn    = "${aws_api_gateway_rest_api.paradise_cakes_api.execution_arn}/*"
-}
