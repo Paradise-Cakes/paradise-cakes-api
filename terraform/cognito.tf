0ee4a8d16cb970487ec4e87a8ee4dd7d5c22e6a6
Anthony Viera <anthonyviera4@gmail.com>
2024-08-24 22:22:20 -0500

    Will this work

--- terraform/cognito.tf
+++ terraform/cognito.tf
@@ -1,78 +1,96 @@
-# resource "aws_cognito_user_pool_client" "paradise_cakes_client" {
-#   name         = "paradise-cakes-client"
-#   user_pool_id = aws_cognito_user_pool.paradise_cakes_user_pool.id
+resource "aws_cognito_user_pool_client" "paradise_cakes_client" {
+  name         = "paradise-cakes-client"
+  user_pool_id = aws_cognito_user_pool.paradise_cakes_user_pool.id
 
-#   generate_secret = false
+  generate_secret = false
 
-#   explicit_auth_flows = [
-#     "ALLOW_USER_SRP_AUTH",
-#     "ALLOW_REFRESH_TOKEN_AUTH",
-#     "ALLOW_USER_PASSWORD_AUTH",
-#   ]
-# }
+  explicit_auth_flows = [
+    "ALLOW_USER_SRP_AUTH",
+    "ALLOW_REFRESH_TOKEN_AUTH",
+    "ALLOW_USER_PASSWORD_AUTH",
+  ]
+}
 
-# resource "aws_cognito_user_pool" "paradise_cakes_user_pool" {
-#   name = "paradise-cakes-user-pool"
+resource "aws_cognito_user_pool" "paradise_cakes_user_pool" {
+  name = "paradise-cakes-user-pool"
 
-#   password_policy {
-#     minimum_length    = 8
-#     require_lowercase = true
-#     require_numbers   = true
-#     require_symbols   = true
-#     require_uppercase = true
-#   }
+  password_policy {
+    minimum_length    = 8
+    require_lowercase = true
+    require_numbers   = true
+    require_symbols   = true
+    require_uppercase = true
+  }
 
-#   username_attributes        = ["email"]
-#   auto_verified_attributes   = ["email"]
-#   sms_authentication_message = "Your code is {####}"
+  username_attributes        = ["email"]
+  auto_verified_attributes   = ["email"]
+  sms_authentication_message = "Your code is {####}"
 
-#   device_configuration {
-#     challenge_required_on_new_device      = true
-#     device_only_remembered_on_user_prompt = true
-#   }
+  device_configuration {
+    challenge_required_on_new_device      = true
+    device_only_remembered_on_user_prompt = true
+  }
 
-#   email_configuration {
-#     email_sending_account = "DEVELOPER"
-#     source_arn            = "arn:aws:ses:us-east-1:${data.aws_caller_identity.current.account_id}:identity/do-not-reply@paradisecakesbymegan.com"
-#     from_email_address    = "do-not-reply@paradisecakesbymegan.com"
-#   }
+  email_configuration {
+    email_sending_account = "DEVELOPER"
+    source_arn            = "arn:aws:ses:us-east-1:${data.aws_caller_identity.current.account_id}:identity/do-not-reply@paradisecakesbymegan.com"
+    from_email_address    = "do-not-reply@paradisecakesbymegan.com"
+  }
 
-#   schema {
-#     name                = "given_name"
-#     attribute_data_type = "String"
-#     mutable             = true
-#     required            = false
-#     string_attribute_constraints {
-#       min_length = "1"
-#       max_length = "50"
-#     }
-#   }
+  schema {
+    name                = "given_name"
+    attribute_data_type = "String"
+    mutable             = true
+    required            = false
+    string_attribute_constraints {
+      min_length = "1"
+      max_length = "50"
+    }
+  }
 
-#   schema {
-#     name                = "family_name"
-#     attribute_data_type = "String"
-#     mutable             = true
-#     required            = false
-#     string_attribute_constraints {
-#       min_length = "1"
-#       max_length = "50"
-#     }
-#   }
+  schema {
+    name                = "family_name"
+    attribute_data_type = "String"
+    mutable             = true
+    required            = false
+    string_attribute_constraints {
+      min_length = "1"
+      max_length = "50"
+    }
+  }
 
-#   account_recovery_setting {
-#     recovery_mechanism {
-#       name     = "verified_email"
-#       priority = 1
-#     }
-#     recovery_mechanism {
-#       name     = "verified_phone_number"
-#       priority = 2
-#     }
-#   }
-# }
+  account_recovery_setting {
+    recovery_mechanism {
+      name     = "verified_email"
+      priority = 1
+    }
+    recovery_mechanism {
+      name     = "verified_phone_number"
+      priority = 2
+    }
+  }
+}
 
-# resource "aws_cognito_user_group" "paradise_cakes_admin_group" {
-#   name         = "paradise-cakes-admin-group"
-#   description  = "paradise cakes admin group"
-#   user_pool_id = aws_cognito_user_pool.paradise_cakes_user_pool.id
-# }
+resource "aws_cognito_identity_pool" "orders_identity_pool" {
+  identity_pool_name               = "orders-identity-pool"
+  allow_unauthenticated_identities = false
+
+  cognito_identity_providers {
+    client_id               = aws_cognito_user_pool_client.paradise_cakes_client.id
+    provider_name           = aws_cognito_user_pool.paradise_cakes_user_pool.provider_name
+    server_side_token_check = false
+  }
+}
+
+resource "aws_cognito_user_group" "paradise_cakes_admin_group" {
+  name         = "paradise-cakes-admin-group"
+  description  = "paradise cakes admin group"
+  user_pool_id = aws_cognito_user_pool.paradise_cakes_user_pool.id
+}
+
+resource "aws_cognito_user_group" "paradise_cakes_user_group" {
+  name         = "paradise-cakes-user-group"
+  description  = "paradise cakes user group"
+  user_pool_id = aws_cognito_user_pool.paradise_cakes_user_pool.id
+}
+
--- terraform/iam.tf
+++ terraform/iam.tf
@@ -78,3 +78,106 @@ resource "aws_iam_role_policy_attachment" "api_gateway_attachment" {
   policy_arn = aws_iam_policy.paradise_cakes_api_policy.arn
   role       = aws_iam_role.lambda_execution_role.name
 }
+
+resource "aws_iam_policy_document" "user_assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    effect  = "Allow"
+
+    principals {
+      type        = "Federated"
+      identifiers = ["cognito-idp.amazonaws.com"]
+    }
+  }
+
+  condition {
+    test     = "StringEquals"
+    variable = "cognito-identity.amazonaws.com:aud"
+    values   = [aws_cognito_identity_pool.orders_identity_pool.id]
+  }
+}
+
+resource "aws_iam_policy_document" "admin_assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+
+    effect = "Allow"
+
+    principals {
+      type        = "Federated"
+      identifiers = ["cognito-identity.amazonaws.com"]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "cognito-identity.amazonaws.com:aud"
+      values   = [aws_cognito_identity_pool.orders_identity_pool.id]
+    }
+  }
+}
+
+resource "aws_iam_role" "user_role" {
+  name               = "user-role"
+  assume_role_policy = aws_iam_policy_document.user_assume_role_policy.json
+}
+
+resource "aws_iam_role" "admin_role" {
+  name               = "admin-role"
+  assume_role_policy = aws_iam_policy_document.admin_assume_role_policy.json
+}
+
+resource "aws_iam_policy" "user_order_access_policy" {
+  name        = "user-order-access-policy"
+  description = "Policy for users to access their own orders in DynamoDB"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "dynamodb:GetItem",
+          "dynamodb:PutItem",
+          "dynamodb:UpdateItem"
+        ]
+        Resource = "arn:aws:dynamodb:us-east-1:${data.aws_caller_identity.current.account_id}:table/orders"
+        Condition = {
+          "ForAllValues:StringEquals" = {
+            "dynamodb:LeadingKeys" = [format("cognito-identity.amazonaws.com:%s", "sub")]
+          }
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy" "admin_order_access_policy" {
+  name        = "admin-order-access-policy"
+  description = "Policy for admins to access all orders in DynamoDB"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "dynamodb:Scan",
+          "dynamodb:GetItem",
+          "dynamodb:PutItem",
+          "dynamodb:UpdateItem"
+        ]
+        Resource = "arn:aws:dynamodb:us-east-1:${data.aws_caller_identity.current.account_id}:table/orders"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "user_order_access_attachment" {
+  policy_arn = aws_iam_policy.user_order_access_policy.arn
+  role       = aws_iam_role.user_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "admin_order_access_attachment" {
+  policy_arn = aws_iam_policy.admin_order_access_policy.arn
+  role       = aws_iam_role.admin_role.name
+}
