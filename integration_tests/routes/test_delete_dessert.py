949e25f6167133a326f06b2bfa2e768e68f2766e
Anthony Viera <anthonyviera96@gmail.com>
2024-12-07 17:07:38 -0600

    cmon

--- integration_tests/routes/test_delete_dessert.py
+++ integration_tests/routes/test_delete_dessert.py
@@ -8,7 +8,48 @@ def test_delete_v1_desserts_delete_dessert_returns_200(
 
     assert response.status_code == 200
     assert response.headers.get("Content-Type") == "application/json"
-    assert response.json() == {}
+    assert response.json() == {
+        "dessert_id": dessert_id,
+        "name": "INT_TEST_DESSERT_NAME",
+        "description": "INT_TEST_DESCRIPTION",
+        "dessert_type": "cake",
+        "created_at": response.json().get("created_at"),
+        "last_updated_at": response.json().get("last_updated_at"),
+        "visible": False,
+        "prices": [
+            {
+                "dessert_id": dessert_id,
+                "size": "slice",
+                "base_price": 5.00,
+                "discount": 0.00,
+            },
+            {
+                "dessert_id": dessert_id,
+                "size": "whole",
+                "base_price": 40.00,
+                "discount": 0.00,
+            },
+        ],
+        "ingredients": ["flour", "sugar", "cocoa", "butter", "eggs"],
+        "images": [
+            {
+                "image_id": "IMAGE-1",
+                "url": "https://example.com/image1.jpg",
+                "upload_url": "https://example.com/upload-url",
+                "position": 1,
+                "file_name": "image1.jpg",
+                "file_type": "jpg",
+            },
+            {
+                "image_id": "IMAGE-2",
+                "url": "https://example.com/image2.jpg",
+                "upload_url": "https://example.com/upload-url",
+                "position": 2,
+                "file_name": "image2.jpg",
+                "file_type": "jpg",
+            },
+        ],
+    }
 
     # Confirm dessert is deleted
     response = request_helper.get(f"/v1/desserts/{dessert_id}")
