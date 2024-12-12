af6348acb3b0c06f1e17959f594adfe9d380d236
Anthony Viera <anthonyviera4@gmail.com>
2024-12-12 00:30:55 -0600

    moar int

--- integration_tests/routes/test_get_orders.py
+++ integration_tests/routes/test_get_orders.py
@@ -1,10 +1,94 @@
-def test_get_v1_orders_get_orders_returns_200(
+def test_get_v1_orders_get_orders_by_delivery_date_returns_200(
     request_helper, function_orders, cleanup_orders
 ):
     order_ids = [order_id for order_id in function_orders["order_ids"]]
     print(order_ids)
 
     response = request_helper.get("/v1/orders?delivery_date=01-01-2022")
+    response.raise_for_status()
+    cleanup_orders.extend([{"order_id": order_id} for order_id in order_ids])
+
+    assert response.status_code == 200
+    assert response.headers.get("Content-Type") == "application/json"
+    assert response.json() == [
+        {
+            "order_id": order_ids[0],
+            "customer_first_name": "John",
+            "customer_last_name": "Cena",
+            "customer_full_name": "John Cena",
+            "customer_email": "john.cena@gmail.com",
+            "customer_phone_number": "1234567890",
+            "delivery_zip_code": "12345",
+            "delivery_address_line_1": "123 Main St",
+            "delivery_address_line_2": "Apt 1",
+            "delivery_date": "01-01-2022",
+            "delivery_time": 12,
+            "order_date": "12-31-2021",
+            "order_time": 12,
+            "order_status": "NEW",
+            "approved": False,
+            "custom_order": False,
+            "order_total": 0,
+            "desserts": [
+                {
+                    "dessert_id": "DESSERT-1",
+                    "dessert_name": "Chocolate Cake",
+                    "size": "slice",
+                    "quantity": 2,
+                }
+            ],
+        }
+    ]
+
+def test_get_v1_orders_get_orders_by_customer_full_name_returns_200(
+    request_helper, function_orders, cleanup_orders
+):
+    order_ids = [order_id for order_id in function_orders["order_ids"]]
+    print(order_ids)
+
+    response = request_helper.get("/v1/orders?customer_full_name=John Cena")
+    response.raise_for_status()
+    cleanup_orders.extend([{"order_id": order_id} for order_id in order_ids])
+
+    assert response.status_code == 200
+    assert response.headers.get("Content-Type") == "application/json"
+    assert response.json() == [
+        {
+            "order_id": order_ids[0],
+            "customer_first_name": "John",
+            "customer_last_name": "Cena",
+            "customer_full_name": "John Cena",
+            "customer_email": "john.cena@gmail.com",
+            "customer_phone_number": "1234567890",
+            "delivery_zip_code": "12345",
+            "delivery_address_line_1": "123 Main St",
+            "delivery_address_line_2": "Apt 1",
+            "delivery_date": "01-01-2022",
+            "delivery_time": 12,
+            "order_date": "12-31-2021",
+            "order_time": 12,
+            "order_status": "NEW",
+            "approved": False,
+            "custom_order": False,
+            "order_total": 0,
+            "desserts": [
+                {
+                    "dessert_id": "DESSERT-1",
+                    "dessert_name": "Chocolate Cake",
+                    "size": "slice",
+                    "quantity": 2,
+                }
+            ],
+        }
+    ]
+
+def test_get_v1_orders_get_orders_by_order_date_returns_200(
+    request_helper, function_orders, cleanup_orders
+):
+    order_ids = [order_id for order_id in function_orders["order_ids"]]
+    print(order_ids)
+
+    response = request_helper.get("/v1/orders?order_date=12-31-2021")
     response.raise_for_status()
     cleanup_orders.extend([{"order_id": order_id} for order_id in order_ids])
 
