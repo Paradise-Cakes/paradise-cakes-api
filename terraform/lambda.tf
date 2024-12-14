7a407bb33a544cd314705622c393d83473e58439
Anthony Viera <anthonyviera4@gmail.com>
2024-12-14 01:12:34 -0600

    lambda perm

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -52,3 +52,12 @@ resource "aws_lambda_function" "customize_emails_trigger" {
     }
   }
 }
+
+resource "aws_lambda_permission" "allow_cognito_invocation" {
+  statement_id  = "AllowCognitoInvocation"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.customize_emails_trigger.function_name
+  principal     = "cognito-idp.amazonaws.com"
+  source_arn    = aws_cognito_user_pool.paradise_cakes_user_pool.arn
+}
+
