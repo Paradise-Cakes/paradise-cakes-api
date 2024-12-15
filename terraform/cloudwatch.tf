c0714eb3c3799724b8ce0acd1bc24e52eaaa4bad
Anthony Viera <anthonyviera4@gmail.com>
2024-12-14 20:58:11 -0600

    stop burning money on logs

---
+++ terraform/cloudwatch.tf
@@ -0,0 +1,13 @@
+resource "aws_cloudwatch_log_group" "app" {
+  name              = "/aws/lambda/${aws_lambda_function.app.function_name}"
+  retention_in_days = 3
+
+  depends_on = [aws_lambda_function.app]
+}
+
+resource "aws_cloudwatch_log_group" "customize_emails_trigger" {
+  name              = "/aws/lambda/${aws_lambda_function.customize_emails_trigger.function_name}"
+  retention_in_days = 3
+
+  depends_on = [aws_lambda_function.customize_emails_trigger]
+}
