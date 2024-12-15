6795dc7ecc89d2c0406f90beefa6faaf7f8364dd
Anthony Viera <anthonyviera4@gmail.com>
2024-12-14 21:12:20 -0600

    data sources

--- terraform/cloudwatch.tf
+++ terraform/cloudwatch.tf
@@ -1,13 +1,9 @@
-resource "aws_cloudwatch_log_group" "app" {
+data "aws_cloudwatch_log_group" "app" {
   name              = "/aws/lambda/${aws_lambda_function.app.function_name}"
   retention_in_days = 3
-
-  depends_on = [aws_lambda_function.app]
 }
 
-resource "aws_cloudwatch_log_group" "customize_emails_trigger" {
+data "aws_cloudwatch_log_group" "customize_emails_trigger" {
   name              = "/aws/lambda/${aws_lambda_function.customize_emails_trigger.function_name}"
   retention_in_days = 3
-
-  depends_on = [aws_lambda_function.customize_emails_trigger]
 }
