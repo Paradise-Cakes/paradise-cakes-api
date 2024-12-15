7c16dd36d177429fd12dca69fdb802c0c90aec94
Anthony Viera <anthonyviera4@gmail.com>
2024-12-14 21:34:44 -0600

    import state temporarily

--- terraform/cloudwatch.tf
+++ terraform/cloudwatch.tf
@@ -1,9 +1,9 @@
-data "aws_cloudwatch_log_group" "app" {
+resource "aws_cloudwatch_log_group" "app" {
   name              = "/aws/lambda/${aws_lambda_function.app.function_name}"
   retention_in_days = 3
 }
 
-data "aws_cloudwatch_log_group" "customize_emails_trigger" {
+resource "aws_cloudwatch_log_group" "customize_emails_trigger" {
   name              = "/aws/lambda/${aws_lambda_function.customize_emails_trigger.function_name}"
   retention_in_days = 3
 }
