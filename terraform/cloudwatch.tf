e922135d607cb766c5e1e8fe4ac0a12866de5f84
Anthony Viera <anthonyviera4@gmail.com>
2024-12-14 21:51:06 -0600

    try this

--- terraform/cloudwatch.tf
+++ terraform/cloudwatch.tf
@@ -1,9 +1,9 @@
 resource "aws_cloudwatch_log_group" "app" {
-  name              = "/aws/lambda/${aws_lambda_function.app.function_name}"
+  name              = "/aws/lambda/paradise-cakes-api-us-east-1"
   retention_in_days = 3
 }
 
 resource "aws_cloudwatch_log_group" "customize_emails_trigger" {
-  name              = "/aws/lambda/${aws_lambda_function.customize_emails_trigger.function_name}"
+  name              = "/aws/lambda/customize-emails-trigger"
   retention_in_days = 3
 }
